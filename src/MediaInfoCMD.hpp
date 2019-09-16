78e108610e1b4c1b760647aa2dbc7da998d7186f
Tyler Bobik <shed412@gmail.com>
2019-09-16 18:11:22 -0400

    made header for media info class

--- src/MediaInfoCMD.hpp
+++ src/MediaInfoCMD.hpp
@@ -2,13 +2,30 @@
 #define MEDIA_CMD
 
 #include "ExecCMD.hpp"
+#include <map>
 
 class MediaInfoCMD : public ExecCMD {
+    
+    private:
+    bool executed = false;
+    map<string, string> general;
+    map<string, string> video;
+    map<string, string> audio;
+    map<string, string> text;
 
+    /**
+     * I'll want to make the parsing happen in the exec
+     * Call super exec first, then parse for maps
+    */
     public:
         MediaInfoCMD() : ExecCMD(){}
         MediaInfoCMD(vector<string> args) : ExecCMD(args){}
         MediaInfoCMD(string args) : ExecCMD(args){}
+        map<string, string> GetGeneralInfo();
+        map<string, string> GetVideoInfo();
+        map<string, string> GetAudioInfo();
+        map<string, string> GetTextInfo();
+         
 
 };
 
